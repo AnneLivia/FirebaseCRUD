a2370554b95757ed45e24ddefca2d36d994d5b26
Anne Livia <annelivia16@gmail.com>
2022-07-07 11:32:36 -0300

    joi and validation middleware first setup

---
+++ src/validators/Schema.js
@@ -0,0 +1,11 @@
+import Joi from 'joi';
+import UserModel from '../models/UserModel.js';
+import NoteModel from '../models/NoteModel.js';
+
+const SchemaUser = Joi.object({
+    
+});
+
+const SchemaNote = Joi.object({
+
+})
