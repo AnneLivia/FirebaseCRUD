432f132a7b12397d403c52b9b1d200564484aebe
Anne Livia <annelivia16@gmail.com>
2022-07-07 11:33:18 -0300

    router updated

--- src/routes/NoteRouter.js
+++ src/routes/NoteRouter.js
@@ -2,6 +2,9 @@ import RouterClass from './RouterClass.js';
 
 import NoteController from "../controllers/NoteController.js";
 
+const noteController = new NoteController();
+
 const router = new RouterClass('/notes', NoteController);
+router.router.get('/notes/all/:userId', noteController.getByUser.bind(noteController))
 
 export default router.router;
